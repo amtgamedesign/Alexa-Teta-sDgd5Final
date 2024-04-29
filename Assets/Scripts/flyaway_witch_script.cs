479f235b1515c666ca29331ba844f424e5e4d33e
ALEXA\Ateta <amtgamedesign@gmail.com>
2024-04-29 13:39:22 -0400

    Push town triangle

---
+++ Assets/Scripts/flyaway_witch_script.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flyaway_witch_script : MonoBehaviour
+{
+    public Rigidbody fwrb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("speed", 2);
+        Invoke("selfdestruct", 7);
+    }
+
+    public void speed()
+    {
+        fwrb.velocity = new Vector3(-10,4);
+    }
+    public void selfdestruct()
+    {
+        Destroy(gameObject);
+    }
+   
+}
